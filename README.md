a7552bc842de9a03abbd1b05a3ff84763f496e51
Yuriy Bereza <ybereza@gmail.com>
2020-11-14 21:17:03 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Service Application Runner (SAR)
+
+This is a simple tool to run windows applications as a system service. It is a replacement for srvany.exe from Windows NT Resource Kit as it is no longer available.
+
+I have used it to run [Syncthing](https://syncthing.net/) windows console application, but it may be used to run any other application.
+
+## Getting started
+
+Download SAR.zip from "Releases" tab and extract SAR.exe from archive into any destination directory. For example C:\Users\<UserName>\tools\SAR
+
+## Creating service
+
+Run PowerShell as System Administrator (Righ click on "Start" button and select Windows PowerShell(Admin))
+
+![ps](./doc/images/ps.png)
+
+* Go to directory with extracted SAR.exe
+* Run cmdlet `New-Service <ServiceName> -BinaryPathName "C:\Users\<UserName>\tools\SAR\SAR.exe <Path to application with param>"
+
+Example:
+
+```
+New-Service "Syncthing" -BinaryPathName "C:\Users\ybereza\tools\SAR\SAR.exe C:\Users\ybereza\tools\syncthing.exe -no-browser -no-console -no-restart -logfile C:\Users\ybereza\tools\syncthing\log.txt"
+```
+
+Now you can start your service from "Services" Application.
+
+![services](./doc/images/services.png)
+
+## Troubleshooting
+
+If there was an error during application start, it will be shown in System Event Log. Press "Start" button and type "Event Viewer", now go to Applications and System Services log and locate "Service Application Runner" log file
+
+![events](./doc/images/events.png)
---
+++ SAR.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Diagnostics;
+using System.ServiceProcess;
+
+namespace SAR
+{
+    public class SAR : ServiceBase
+    {
+        private const string LogName = "Service Application Runner";
+        private const string LogSrc  = "Service Application Runner";
+
+        private IntPtr currentProcHandle = IntPtr.Zero;
+        private EventLog eventLog;
+        public SAR()
+        {
+            if (!EventLog.SourceExists(LogSrc))
+            {
+                EventLog.CreateEventSource(LogSrc, LogName);
+            }
+            eventLog = new EventLog(LogName)
+            {
+                Source = LogSrc
+            };
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            string[] cmdArgs = Environment.GetCommandLineArgs();
+            if (cmdArgs.Length < 2)
+            {
+                var message = "SAR Can not start process: too few arguments specified";
+                eventLog.WriteEntry(message, EventLogEntryType.Error);
+                throw new Exception(message);
+            }
+            string[] processWithArgs = new string[cmdArgs.Length - 1];
+            Array.Copy(cmdArgs, 1, processWithArgs, 0, processWithArgs.Length);
+            string cmd = string.Join(" ", processWithArgs);
+            var info = ProcessCreator.CreateProcess(0, cmd);
+            if (info.ProcHandle == IntPtr.Zero)
+            {
+                string message = "SAR can not start process: " + cmd;
+                eventLog.WriteEntry(message, EventLogEntryType.Error);
+                throw new Exception(message);
+            }
+            else
+            {
+                currentProcHandle = info.ProcHandle;
+                eventLog.WriteEntry(string.Format("SAR started process {0} with PID {1}", info.ProcName, info.PID), EventLogEntryType.Information);
+            }
+        }
+
+        protected override void OnStop()
+        {
+            if (currentProcHandle != IntPtr.Zero) {
+                ProcessCreator.StopProcess(currentProcHandle);
+                currentProcHandle = IntPtr.Zero;
+            }
+        }
+    }
+}
